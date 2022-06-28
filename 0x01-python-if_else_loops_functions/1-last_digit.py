16dfe037ff7ba6d383cc48d462c9be4ea11e656f
Rickyflame <kimumuricky@gmail.com>
2022-06-28 12:28:34 +0300

    This program will assign a random signed number to the variable number each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable number.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number < 0:
     last_digit = -last_digit
 print('last digit of {} is {} and is'.format(number, last_digit), end='')
 if last_digit > 5:
-    print('greater than 5')
+    print(' greater than 5')
 elif last_digit == 0:
-    print('0')
+    print(' 0')
 else:
-    print('less than 6 and not 0')
+    print(' less than 6 and not 0')
