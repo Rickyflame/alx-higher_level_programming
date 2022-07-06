f08df2a772b2de2409aef987f8d26584945cab46
Rickyflame <kimumuricky@gmail.com>
2022-07-06 15:36:57 +0300

    Create a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if i > 0 and rom_val[roman_string[i]] > rom_val[roman_string[i - 1]]:
             int_val += rom_val[roman_string[i]] - 2 * \
-            rom_val[roman_string[i - 1]]
+                        rom_val[roman_string[i - 1]]
         else:
             int_val += rom_val[roman_string[i]]
     return int_val
