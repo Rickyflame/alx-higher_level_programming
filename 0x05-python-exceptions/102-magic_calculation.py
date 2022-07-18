d16bcb28c5e906debf9e0051a60a50099bb24ccd
Rickyflame <kimumuricky@gmail.com>
2022-07-18 16:32:28 +0300

    Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
+def safe_print_list_integers(my_list=[], x=0):
+    a = 0
+    for i in range(x):
         try:
-            if i > a:
-                raise Exception('Too far')
-            else:
-                result += (a ** b) / i
-        except:
-            result = b + a
-            break
-    return (result)
+            print("{:d}".format(my_list[i]), end="")
+            a += 1
+        except(ValueError, TypeError):
+            continue
+    print()
+    return a
