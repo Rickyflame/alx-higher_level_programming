cc270164137689f012ee2e438667f086d1fdeb90
Rickyflame <kimumuricky@gmail.com>
2022-07-19 15:51:00 +0300

    Write a class Square that defines a square by

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,18 +1,19 @@
-#!/usr/bin/pyhton3
-"""define a class"""
+#!/usr/bin/python3
+"""Define a class Square."""
 
 
 class Square:
-    """represent a square"""
-    def __init__(self, size=0):
-        """Initialize a new square
-        args: size (int): size of new square
+    """Represent a square."""
+    def __init__(self, size):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """get the current size of square"""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -24,11 +25,11 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Returns current area of the square"""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """prints the square with # character"""
+        """Print the square with the # character."""
         for i in range(0, self.__size):
             [print("#", end="") for j in range(self.__size)]
             print("")
