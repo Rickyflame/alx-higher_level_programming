106ae6019fa68415221ba93673b401c653edcd94
Rickyflame <kimumuricky@gmail.com>
2022-07-21 12:13:08 +0300

    Write a function that prints first_name and last_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""defines a name printing function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints a name
+    Args:
+    first_name (str): The first name to print
+    last_name (str): The last name to print
+    Raises:
+    TypeError: if either first_name or last_name is not a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(My name is {} {}".format(first_name, last_name))
