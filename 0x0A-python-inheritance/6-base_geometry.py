a78f18517a39fc04fd861ec1a4b529ecde9b0111
Rickyflame <kimumuricky@gmail.com>
2022-08-01 13:23:49 +0300

    Write a class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    """function raises an exception"""
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
