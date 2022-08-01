1f4803afbd9cca05182fdfc251a085925a18b4ac
Rickyflame <kimumuricky@gmail.com>
2022-08-01 14:54:29 +0300

    Write a class Rectangle that inherits from BaseGeometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,6 +15,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """represents a rectangle"""
     def __init__(self, width, height):
