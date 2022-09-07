0180c20c26adb1b3627b7decaa8b580fca57ad65
Rickyflame <kimumuricky@gmail.com>
2022-09-07 21:08:10 +0300

    Write a script that creates the MYSQL server

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates user_0d_1 with privileges on mysql server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
