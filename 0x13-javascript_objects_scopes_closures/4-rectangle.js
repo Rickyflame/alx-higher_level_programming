ba6b031826fd9d4977e554f1720232f30dd22ecc
Rickyflame <kimumuricky@gmail.com>
2022-10-04 14:14:48 +0300

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -4,15 +4,15 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-    print () {
-      for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
 
-    rotate () {
-      [this.width, this.height] = [this.height, this.width];
-    }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
 
-    double () {
-      [this.width, this.height] = [this.width * 2, this.height * 2];
-    }
+  double () {
+    [this.width, this.height] = [this.width * 2, this.height * 2]; 
+  }
 };
