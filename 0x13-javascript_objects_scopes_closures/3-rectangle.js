ae5b8db9274fe9fc06b55a8196812127fb41b8a2
Rickyflame <kimumuricky@gmail.com>
2022-10-04 13:15:18 +0300

    tasks 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,6 +5,6 @@ module.exports = class Rectangle {
   }
 
   print () {
-	  for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
