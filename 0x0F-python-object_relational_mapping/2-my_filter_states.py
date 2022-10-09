82773fb0f3a72211ded4df2783cbb3a548947423
Rickyflame <kimumuricky@gmail.com>
2022-10-09 21:09:28 +0300

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values in the states
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states
+        WHERE CAST(name AS BINARY) = CAST('[]' AS BINARY)
+        ORDER BY states.id;""".format(sys.argv[4]))
+    records = cursor.fetchall()
+    for record in records:
+        print(record)
+    db.close()
