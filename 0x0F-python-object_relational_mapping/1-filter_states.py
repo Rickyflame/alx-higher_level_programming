7e6c87166220e7261b43f929a47dea20f2e8ca87
Rickyflame <kimumuricky@gmail.com>
2022-10-09 21:16:24 +0300

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) >= 4:
         db = MySQLdb.connect(
             host='localhost',
-            user=sys.argv[1].
+            user=sys.argv[1],
             passwd=sys.argv[2],
             db=sys.argv[3],
             port=3306
