1ed91a90a3b50c91f9ad0febc0a24b6961ce20e8
Rickyflame <kimumuricky@gmail.com>
2022-10-09 20:56:35 +0300

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Lists all the states with a name starting wwith N from the database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 4:
+        db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1].
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+        cursor = db.cursor()
+        cursor.execute("""
+        SELECT * FROM states
+        WHERE name IS  NOT NULL
+        AND LEFT(CAST(name AS BINARY), 1) ='N'
+        ORDER BY states.id;
+        """)
+        records = cursor.fetchall()
+        for record in records:
+            print(record)
+        db.close()
