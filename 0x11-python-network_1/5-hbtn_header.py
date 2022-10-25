b5a406d88e75889af1ee7bb4ea27e335412fc571
Rickyflame <kimumuricky@gmail.com>
2022-10-25 10:21:34 +0300

    task

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+- Takes in a URL
+- Sends a request to the URL and displays the value of the variable
+- X-Request-Id in the response header
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    print(req.headers.get("X-Request-Id"))
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+- Takes in a URL and an email address
+- Sends a POST request to the passed URL with the email as a parameter
+- Displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
